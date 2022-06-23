4f6159fdf510a06a1a824676899c5defe2a8e4db
wonhyejong <whj0524@naver.com>
2022-06-24 08:42:16 +0900

    start

---
+++ src/components/Plus.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+        <h1> 값 : </h1>
+        <button>
+          plus + 
+        </button>
+      </div>
+    );
+};
+
+export default Counter;
